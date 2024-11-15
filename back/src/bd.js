5d3477febee09c59da28136fabbb908ec94b3cb7
gooigi <lopez.gallego2611@gmail.com>
2024-11-15 09:53:46 -0500

    commit

--- back/src/bd.js
+++ back/src/bd.js
@@ -2,6 +2,7 @@
 /* instanciar la libreria mysql */
 
 const mysql = require("mysql2"); /* principio de inmutabilidad */
+const aprendiz = require("./aprendiz");
 
 /* cadena de conexión o string de conexión */
 
@@ -23,6 +24,67 @@ cnx.connect((error) => {
   } else {
     console.log("conexión existosa a la BD");
   }
+});
+
+// autenticacion de un factor
+
+aprendiz.post("/users/login", (req, res) => {
+  //datos de la peticion (body)
+  let email = req.body.email;
+  let password = req.body.password;
+
+  //validamos que la data esté completa
+  if (!email || !password) {
+    res.status(400).send({
+      consulta: "error",
+      mensaje: "faltan datos por enviar del formulario ! ",
+    });
+  }
+
+  let correo;
+  let pass;
+  let lasname;
+  let name;
+
+  // buscar en la bd el usuario  y validar
+  bd.query(
+    "SELECT name, lastname, email, password FROM users WHERE email like ?",
+    [email],
+    (error, consulta) => {
+      consulta.forEach((element) => {
+        // seteamos las varuables con el resultado de la consulta
+        pass = element.password;
+        correo = element.email;
+        name = element.name;
+        lasname = element.lastname;
+      });
+
+      // validación 1; email corresponde
+      if (correo == null) {
+        res.status(400).send({
+          status: "error",
+          mensaje: "Usuario no existe en la BD",
+        });
+      }
+      console.log(password);
+      console.log(pass);
+      let pwd = bcrypt.compareSync(password, pass);
+      console.log(pwd);
+      if (!pwd) {
+        res.status(400).send({
+          status: "error",
+          mensaje: "Pwd Incorrecto !",
+        });
+      } else {
+        res.status(200).send({
+          consulta: "ok",
+          mensaje: "Ingreso exitoso al sistema!",
+          user: name + " " + lasname,
+          email: email,
+        });
+      }
+    }
+  );
 });
 
 module.exports = cnx;
